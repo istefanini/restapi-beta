3c52f97ac779c50fbfa09226eb475f4f61b056a4
Igna <istefanini@fleni.org.ar>
2022-07-13 12:07:52 -0300

    cambio de ruta en post

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", handlers.IndexRoute)
-	router.HandleFunc("/payment/api/v1/notificaction-mol-payment ", handlers.CreatePayment).Methods("POST")
+	router.HandleFunc("/payment/api/v1/notificaction-mol-payment", handlers.CreatePayment).Methods("POST")
 	router.HandleFunc("/payment/api/v1/payments", handlers.GetPayments).Methods("GET")
 	router.HandleFunc("/payment/api/v1/payments/{external_reference}", handlers.GetOnePayment).Methods("GET")
 	fmt.Println("Server corriendo en puerto", 3000)
