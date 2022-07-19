43366a10d8fdcc6ee60732bfca54b55545753318
Igna <istefanini@fleni.org.ar>
2022-07-19 14:38:14 -0300

    cambios

--- main.go
+++ main.go
@@ -7,34 +7,30 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/subosito/gotenv"
-
 	"github.com/gorilla/mux"
 	"github.com/istefanini/restapi-beta/api"
+	"github.com/subosito/gotenv"
 )
 
 func IndexRoute(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Rest api funcionando")
 }
 
-func init() {
-	gotenv.Load()
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode("alive true")
 }
 
-func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    json.NewEncoder(w).Encode("alive true")
+func init() {
+	gotenv.Load()
 }
 
 func main() {
 
 	log.Println(os.Getenv("x-api-key"))
-
 	r := mux.NewRouter()
-
 	a := &api.API{}
-
 	a.RegisterRoutes(r)
 
 	r.HandleFunc("/", IndexRoute).Methods(http.MethodGet)
